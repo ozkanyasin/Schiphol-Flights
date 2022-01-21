682c6a487654e2fa0a0ca02e5398d272c09d31a0
pirematur3 <36839831+pirematur3@users.noreply.github.com>
2022-01-21 14:13:04 +0300

    Schiphol-Flights App

---
+++ app/src/main/java/com/example/schipholflights/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.schipholflights.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.schipholflights.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
